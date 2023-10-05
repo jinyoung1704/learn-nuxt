022626fddb961cafcbd332e9e6afc4d28194bee8
jinyoung1704 <jinyoung1704@naver.com>
2023-10-05 14:12:52 +0900

    섹션1 라우팅 관련 컴포넌트 정리까지

---
+++ pages/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <NuxtLogo />
+        <h1>메인 페이지</h1>
+    </div>
+</template>
+
+<script>
+    import NuxtLogo from '@/components/NuxtLogo.vue'
+    export default {
+        comments:{
+            NuxtLogo,
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ pages/product/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        프로덕트 페이지
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
