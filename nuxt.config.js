f5c26f6f0e8bbfa36a851dab61698fe17bf9bd33
jinyoung1704 <jinyoung1704@naver.com>
2023-10-06 11:19:04 +0900

    상세페이지까지 연결

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/reset.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -33,5 +33,12 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: [({ isLegacy }) => isLegacy && 'axios']
+  },
+
+  //server setup
+  server: {
+    port : 5000,
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,92 @@
 <template>
-  <div>
-    <h1>홈페이지</h1>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+      <!-- <NuxtLogo /> -->
+      <main>
+        <div>
+          <input type="text" />
+        </div>
+        <div>
+            <ul>
+                <li  v-for="product in products" :key="product.id" class="item flex"
+                @click="moveToDetailPage(product.id)">
+                  <img class="product-image" 
+                  :src="product.imgUrl" 
+                  :alt="product.name" />
+                  <p> {{ product.name }}</p>
+                  <span> {{ product.price }}</span>
+                </li>
+            </ul>
+        </div>
+      </main>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import axios from 'axios'
+
+  export default {
+      async asyncData(){
+          const response = await axios.get('http://localhost:3000/products')
+          console.log(response)
+          const products = response.data.map((item) =>{
+             return {
+              ...item,
+              imageUrl:  `${item.imgUrl}?random=${Math.random()}`
+             }
+          })
+          return {products}
+      },
+
+      methods:{
+        moveToDetailPage(id){
+          console.log(id);
+          this.$router.push(`detail/${id}`);
+
+        }
+      }
+
+      // data(){
+      //     return {
+      //         products: [],
+      //     }
+      // },
+
+      // async created(){
+         
+      // }
+  }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
